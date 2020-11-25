e902a905cec17c13f1c3b14576e767d1a43bb6da
Benjamin Pachev <bap2934@tuxedo.ices.utexas.edu>
2020-11-25 13:39:20 -0600

    More progress on adaptive CSR algorithm.

--- power_iteration_pr.cpp
+++ power_iteration_pr.cpp
@@ -12,6 +12,26 @@
 
 namespace sycl = cl::sycl;
 
+void normalize_weights(float * weights, int n) {
+    float norm = 0.0;
+    int i;
+    for (i = 0; i < n; i++) {
+        norm += weights[i];
+    }
+    for (i = 0; i < n; i++) weights[i] /= norm;
+}
+
+template<typename T> void print_array(T* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+        if ((i == 5) && (i < size-5)) {
+            std::cout << "... ";
+            i = size - 5;
+        }
+    }
+    std::cout << endl;
+}
+
 void scalar_csr(SYCL_CSR_Graph * f)
 {
     SYCL_CSR_Graph* g = f->flip();
@@ -88,10 +108,11 @@ void scalar_csr(SYCL_CSR_Graph * f)
             auto tmp = res_buf;
             res_buf = next_buf;
             next_buf = tmp;
+            break;
         }
     }
 
-    int i;
+/*    int i;
     float one_norm = 0.0;
     // normalize
     for (i = 0; i < n; i++) {
@@ -106,7 +127,13 @@ void scalar_csr(SYCL_CSR_Graph * f)
             cout << "... ";
             i = n - 5;
         }
-    }
+    }*/
+
+    //normalize_weights(next_residuals, n);
+
+    cout << "PageRank weights: " << endl;
+    print_array(next_residuals, n);
+
 
     free(residuals);
     free(next_residuals);
@@ -205,6 +232,9 @@ void adaptive_csr(SYCL_CSR_Graph * f)
     // Try using twice the work group size for the number of nonzeros
     int num_blocks = computeRowBlocks(g->nodeDegree, n, m, block_size, &rowBlocks);
     std::cout << "Number of blocks " << num_blocks << std::endl;
+    std::cout << "Block bounds: ";
+    std:cout << "First block nonzeros: " << g->nodePtr[rowBlocks[1]] - g->nodePtr[rowBlocks[0]] << std::endl;
+    print_array(rowBlocks, num_blocks);
 
     // Begin C scope to enable nice SYCL memory management
     {
@@ -238,18 +268,84 @@ void adaptive_csr(SYCL_CSR_Graph * f)
                         size_t local_id = item.get_local_linear_id();
                         size_t global_id = item.get_global_linear_id();
                         size_t group_id = item.get_group_linear_id();
+
                         if (group_id >= num_blocks) return;
                         int first_node = rowBlocks[group_id];
                         int last_node = rowBlocks[group_id+1];
+                        int block_data_begin = nodePtr[first_node];
+                        int nnz = nodePtr[last_node] - block_data_begin;
+                        int i;
+                        // Load data into cache
+                        for (i=local_id; i < nnz; i += wgroup_size) {
+                            auto src = edgeDst[block_data_begin+i];
+                            local_mem[i] = res[src]/deg[src];
+                        }
+                        // sync threads
+                        item.barrier(sycl::access::fence_space::local_space);
                         // more than one row in the block
                         // we need to apply CSR-stream
-                        if (first_node - last_node > 1) {
+                        if (last_node-first_node > 1) {
+
+                        // Figure out how many threads to use for each row
+                        int t = wgroup_size/(last_node-first_node);
+                        // Get the previous power of two
+                        while (t & (t-1)) t = t & (t-1);
+                        int threads_for_reduction = t;
                         
+                        // reduce each row with multiple threads
+                        if (threads_for_reduction > 1) {
+                            int thread_in_block = local_id % threads_for_reduction;
+                            int local_node = first_node + local_id / threads_for_reduction;
+                            
+                            float sum = 0.0;
+                            if (local_node < last_node) {
+                                int first_edge = nodePtr[local_node] - block_data_begin;
+                                int last_edge = nodePtr[local_node+1] - block_data_begin;
+                                for (i = first_edge; i < last_edge; i += threads_for_reduction) {
+                                    sum += local_mem[i];
+                                }
+                            }
+
+                            // sync threads
+                            item.barrier(sycl::access::fence_space::local_space);
+                            local_mem[local_id] = sum;
+                            
+                            for (int j = threads_for_reduction / 2; j > 0; j /= 2) {
+
+                            // sync threads
+                            item.barrier(sycl::access::fence_space::local_space);
+                            
+                            int use_result = (thread_in_block < j) && ((local_id + j) < block_size);
+                            if (use_result) sum += local_mem[local_id+j];
+
+                            // sync threads
+                            item.barrier(sycl::access::fence_space::local_space);
+
+                            if (use_result) local_mem[local_id] = sum;                            
+                                
+                            }
+
+                            if (thread_in_block == 0 && local_node < last_node) next[local_node] = sum * ALPHA + (1-ALPHA);
+                            //if (group_id == num_blocks-1) next[n-1] = 2;
+                            }
+
+                        // reduce each row with a single thread
+                        else {
+                            for (int local_node = first_node + local_id; local_node < last_node; local_node += wgroup_size) {
+                                float sum = 0.0;
+                                for (int j = nodePtr[local_node]-block_data_begin; j < nodePtr[local_node+1] - block_data_begin; j++) {
+                                    sum += local_mem[j];
+                                }
+                                next[local_node] = ALPHA * sum + (1-ALPHA);
+                            }
+                            //if (group_id == num_blocks-1) next[n-1] = 3;
+                        }
                         }
                         // We are dealing with a single row
                         else {
                            auto row_start = nodePtr[first_node];
                            auto row_end = nodePtr[first_node+1];
+                           //if (group_id == num_blocks-1) next[n-1] = 1;
                            // do the sum
                         }
                         /*float sum = 0.0;
@@ -270,22 +366,10 @@ void adaptive_csr(SYCL_CSR_Graph * f)
         }
     }
 
-    int i;
-    float one_norm = 0.0;
-    // normalize
-    for (i = 0; i < n; i++) {
-        one_norm += next_residuals[i];
-    }
-
+    //normalize_weights(next_residuals, n);
 
     cout << "PageRank weights: " << endl;
-    for (i = 0; i < n; i++) {
-        cout << next_residuals[i]/one_norm << " ";
-        if ((i == 5) && (i < n-5)) {
-            cout << "... ";
-            i = n - 5;
-        }
-    }
+    print_array(next_residuals, n);
 
     free(residuals);
     free(next_residuals);
