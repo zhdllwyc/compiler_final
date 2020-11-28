0ab5fe28aae603878d5f3443c568958891af7845
Benjamin Pachev <bap2934@tuxedo.ices.utexas.edu>
2020-11-28 11:59:22 -0600

    Fixed bugs in Adaptive-CSR implementation. Added more command-line arguments.

--- power_iteration_pr.cpp
+++ power_iteration_pr.cpp
@@ -6,6 +6,7 @@
 
 #define ALPHA 0.85
 #define EPSILON 0.000001
+#define MAX_ITERS 52
 
 // PageRank with power iteration in SYCL
 // This code is intended as a benchmark to test other (hopefully faster) algos against
@@ -32,7 +33,7 @@ template<typename T> void print_array(T* arr, int size) {
     std::cout << endl;
 }
 
-void scalar_csr(SYCL_CSR_Graph * f)
+void scalar_csr(SYCL_CSR_Graph * f, int max_iters=MAX_ITERS)
 {
     SYCL_CSR_Graph* g = f->flip();
     int* outDegree = f->nodeDegree;
@@ -82,7 +83,7 @@ void scalar_csr(SYCL_CSR_Graph * f)
         sycl::buffer<int, 1> nodePtr_buf(g->nodePtr, sycl::range<1>(n+1));
         sycl::buffer<int, 1> edgeDst_buf(g->data, sycl::range<1>(g->numEdges));
 
-        for (int iter = 0; iter < 52; iter++) {
+        for (int iter = 0; iter < max_iters; iter++) {
             queue.submit([&] (sycl::handler& cgh) {
                 auto deg = deg_buf.get_access<sycl::access::mode::read>(cgh);
                 auto nodePtr = nodePtr_buf.get_access<sycl::access::mode::read>(cgh);
@@ -108,32 +109,14 @@ void scalar_csr(SYCL_CSR_Graph * f)
             auto tmp = res_buf;
             res_buf = next_buf;
             next_buf = tmp;
-            break;
         }
     }
 
-/*    int i;
-    float one_norm = 0.0;
-    // normalize
-    for (i = 0; i < n; i++) {
-        one_norm += next_residuals[i];
-    }
-
-
-    cout << "PageRank weights: " << endl;
-    for (i = 0; i < n; i++) {
-        cout << next_residuals[i]/one_norm << " ";
-        if ((i == 5) && (i < n-5)) {
-            cout << "... ";
-            i = n - 5;
-        }
-    }*/
-
-    //normalize_weights(next_residuals, n);
-
-    cout << "PageRank weights: " << endl;
+    cout << "PageRank weights before normalization: " << endl;
     print_array(next_residuals, n);
-
+    normalize_weights(next_residuals, n);
+    cout << "PageRank weights after normalization: " << endl;
+    print_array(next_residuals, n);
 
     free(residuals);
     free(next_residuals);
@@ -145,7 +128,7 @@ void scalar_csr(SYCL_CSR_Graph * f)
 int computeRowBlocks(int * row_lengths, int num_rows, int num_nonzero, int block_size, int** rowBlocks)
 {
     int max_blocks = (num_nonzero+block_size-1) / block_size + 1;
-    int * p = (int*)malloc(sizeof(int)*max_blocks);
+    int * p = (int*)malloc(sizeof(int)*max_blocks*2);
 
     int size = 0;
     int block_num = 0;
@@ -174,16 +157,16 @@ int computeRowBlocks(int * row_lengths, int num_rows, int num_nonzero, int block
 
     // return the pointer
     *rowBlocks = p;
-    if (block_num >= max_blocks) {
+    if (block_num >= 2*max_blocks) {
         std::cout << "Error in block computation, block pointer went out of bounds!" << std::endl;
     }
     // Return the total number of blocks
-    return block_num;
+    return block_num+1;
 }
 
 // Compute power iteration for PageRank using the Adaptive CSR algorithm for sparse matrix-vector multiplication
 // Inspired by https://medium.com/analytics-vidhya/sparse-matrix-vector-multiplication-with-cuda-42d191878e8f
-void adaptive_csr(SYCL_CSR_Graph * f)
+void adaptive_csr(SYCL_CSR_Graph * f, int max_iters=MAX_ITERS)
 {
     SYCL_CSR_Graph* g = f->flip();
     int* outDegree = f->nodeDegree;
@@ -233,8 +216,8 @@ void adaptive_csr(SYCL_CSR_Graph * f)
     int num_blocks = computeRowBlocks(g->nodeDegree, n, m, block_size, &rowBlocks);
     std::cout << "Number of blocks " << num_blocks << std::endl;
     std::cout << "Block bounds: ";
-    std:cout << "First block nonzeros: " << g->nodePtr[rowBlocks[1]] - g->nodePtr[rowBlocks[0]] << std::endl;
     print_array(rowBlocks, num_blocks);
+    std:cout << "First block nonzeros: " << g->nodePtr[rowBlocks[1]] - g->nodePtr[rowBlocks[0]] << std::endl;
 
     // Begin C scope to enable nice SYCL memory management
     {
@@ -247,7 +230,7 @@ void adaptive_csr(SYCL_CSR_Graph * f)
         sycl::buffer<int, 1> edgeDst_buf(g->data, sycl::range<1>(m));
         sycl::buffer<int, 1> rowBlocks_buf(rowBlocks, sycl::range<1>(num_blocks));
 
-        for (int iter = 0; iter < 52; iter++) {
+        for (int iter = 0; iter < max_iters; iter++) {
             queue.submit([&] (sycl::handler& cgh) {
                 auto deg = deg_buf.get_access<sycl::access::mode::read>(cgh);
                 auto nodePtr = nodePtr_buf.get_access<sycl::access::mode::read>(cgh);
@@ -263,28 +246,32 @@ void adaptive_csr(SYCL_CSR_Graph * f)
                      sycl::access::target::local>
                     local_mem(sycl::range<1>(block_size), cgh);
                 cgh.parallel_for<class pagerank_adaptive_iter>(
-                    sycl::nd_range<1>(num_blocks*wgroup_size, wgroup_size),
+                    sycl::nd_range<1>((num_blocks-1)*wgroup_size, wgroup_size),
                     [=] (sycl::nd_item<1> item) {
                         size_t local_id = item.get_local_linear_id();
                         size_t global_id = item.get_global_linear_id();
                         size_t group_id = item.get_group_linear_id();
 
-                        if (group_id >= num_blocks) return;
+                        if (group_id >= num_blocks-1) return;
                         int first_node = rowBlocks[group_id];
                         int last_node = rowBlocks[group_id+1];
                         int block_data_begin = nodePtr[first_node];
                         int nnz = nodePtr[last_node] - block_data_begin;
                         int i;
+                        // more than one node in the block
+                        // we need to apply CSR-stream
+                        if (last_node-first_node > 1) {
+
                         // Load data into cache
+                        // We only do this if there is more than one node
+                        // If there is only one node, we could have more edges than can fit in the cache
                         for (i=local_id; i < nnz; i += wgroup_size) {
                             auto src = edgeDst[block_data_begin+i];
                             local_mem[i] = res[src]/deg[src];
                         }
                         // sync threads
                         item.barrier(sycl::access::fence_space::local_space);
-                        // more than one row in the block
-                        // we need to apply CSR-stream
-                        if (last_node-first_node > 1) {
+
 
                         // Figure out how many threads to use for each row
                         int t = wgroup_size/(last_node-first_node);
@@ -301,7 +288,7 @@ void adaptive_csr(SYCL_CSR_Graph * f)
                             if (local_node < last_node) {
                                 int first_edge = nodePtr[local_node] - block_data_begin;
                                 int last_edge = nodePtr[local_node+1] - block_data_begin;
-                                for (i = first_edge; i < last_edge; i += threads_for_reduction) {
+                                for (i = first_edge+thread_in_block; i < last_edge; i += threads_for_reduction) {
                                     sum += local_mem[i];
                                 }
                             }
@@ -326,7 +313,6 @@ void adaptive_csr(SYCL_CSR_Graph * f)
                             }
 
                             if (thread_in_block == 0 && local_node < last_node) next[local_node] = sum * ALPHA + (1-ALPHA);
-                            //if (group_id == num_blocks-1) next[n-1] = 2;
                             }
 
                         // reduce each row with a single thread
@@ -345,15 +331,25 @@ void adaptive_csr(SYCL_CSR_Graph * f)
                         else {
                            auto row_start = nodePtr[first_node];
                            auto row_end = nodePtr[first_node+1];
-                           //if (group_id == num_blocks-1) next[n-1] = 1;
-                           // do the sum
-                        }
-                        /*float sum = 0.0;
-                        for (auto i = nodePtr[id]; i < nodePtr[id+1]; i++) {
-                            auto src = edgeDst[i];
+                           float sum = 0.0;
+                           for (int j = row_start+local_id; j < row_end; j += wgroup_size) {
+                            auto src = edgeDst[j];
                             sum += res[src]/deg[src];
+                           }
+                           local_mem[local_id] = sum;
+                           for (int k = wgroup_size/2; k > 0; k /= 2) {
+                             item.barrier(sycl::access::fence_space::local_space);
+                            int use_result = (local_id < k) && ((local_id + k) < nnz);
+                            if (use_result) sum += local_mem[local_id+k];
+
+                            // sync threads
+                            item.barrier(sycl::access::fence_space::local_space);
+
+                            if (use_result) local_mem[local_id] = sum;                    
+                           }
+                           // save result
+                           if (local_id == 0) next[first_node] = ALPHA * sum + (1-ALPHA) * sum;
                         }
-                        next[id] = ALPHA * sum + (1-ALPHA);*/
                     }
                 );
 
@@ -362,13 +358,14 @@ void adaptive_csr(SYCL_CSR_Graph * f)
             auto tmp = res_buf;
             res_buf = next_buf;
             next_buf = tmp;
-            break;
         }
     }
 
-    //normalize_weights(next_residuals, n);
 
-    cout << "PageRank weights: " << endl;
+    cout << "PageRank weights before normalization: " << endl;
+    print_array(next_residuals, n);
+    normalize_weights(next_residuals, n);
+    cout << "PageRank weights after normalization: " << endl;
     print_array(next_residuals, n);
 
     free(residuals);
@@ -385,13 +382,26 @@ int main (int argc, char** argv)
         return 1;
     }
 
+    char algo = (argc >= 3) ? argv[2][0] : 'a';
+    int max_iters = (argc >= 4) ? atoi(argv[3]) : MAX_ITERS;
+
     SYCL_CSR_Graph* f = new SYCL_CSR_Graph();
     f->load(argv[1]);
-    //scalar_csr(f);
-    adaptive_csr(f);
+    switch (algo) {
+        case 'a':
+            std::cout << "Running adaptive CSR" << std::endl;
+            adaptive_csr(f, max_iters);
+            break;
+        case 's':
+            std::cout << "Running scalar CSR" << std::endl;
+            scalar_csr(f, max_iters);
+            break;
+        default:
+            std::cout << "Unrecognized algorithm: " << algo << std::endl;
+            break;
+    }
 
     delete f;
     return 0;
 }
-
 
